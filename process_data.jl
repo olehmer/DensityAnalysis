91ea62ce026ab0d9ac37834d7147bc83d6c23509
Owen Lehmer <olehmer@gmail.com>
2016-04-24 22:33:45 -0700

    additional images

--- process_data.jl
+++ process_data.jl
@@ -50,7 +50,7 @@ function findsmallplanets()
   for i=1:length(names)
     emass = planet_masses[i,1]/0.00314647 #mass of earth in MJup units
     density = planet_density[i,1]*1.33 #density of jupiter in g/cm^3
-    if 0.0 < emass < 10.0 && density > 0 #less than 10 earth masses
+    if 0.0 < emass < 10.0 && 20 > density > 0 #less than 10 earth masses
       temp = planetary_temp[i,1]
       name = names[i]
       star_temp = stellar_temp[i,1]
@@ -63,6 +63,7 @@ function findsmallplanets()
       p.stellar_temp = star_temp
       p.temp = temp
       p.distance = dist
+      p.period = orbital_period[i]
       push!(smalls, p)
       #@printf("%2d. %11s Mass=%6.3f (EM), density=%6.3f (g/cm3), planet temp=%4.0f (K), dist=%6.3f (AU), star temp=%4.0f (K)\n",
       #length(smalls), name, emass, density, temp, dist, star_temp)
@@ -74,11 +75,11 @@ end
 planets = findsmallplanets()
 
 #cm = cm.get_cmap('RdYlBu')
-sc = scatter([p.density for p in planets], [p.stellar_temp for p in planets], c=[p.distance for p in planets])
+sc = scatter([p.mass for p in planets], [p.stellar_temp for p in planets], c=[p.density for p in planets])
 cbar = colorbar(sc)
-axis([0, 30, 2500, 6500])
+axis([0, 10, 2500, 6500])
 title("Super Earth Exoplanets Under 10 Earth Masses")
-cbar[:ax][:set_ylabel]("Orbital Radius [AU] (-1=no data)")
-xlabel("Planetary Density [g/cm3]")
+cbar[:ax][:set_ylabel]("Density [g/cm3]")
+xlabel("Mass [Earth Masses]")
 ylabel("Stellar Temperature [K]")
 show()
