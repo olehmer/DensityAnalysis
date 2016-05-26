c91000f58e5010d74501671394b825ffdc8742c7
Owen Lehmer <olehmer@gmail.com>
2016-05-25 22:57:56 -0700

    new images

--- process_data.jl
+++ process_data.jl
@@ -3,11 +3,98 @@ Data taken from: http://www.astro.keele.ac.uk/jkt/tepcat/tepcat.html
 =#
 
 using PyPlot
+using PyCall
+@pyimport matplotlib.colors as COL
 
 G = 6.67E-11 #m3/kg/s2
 M_jup = 1.898E27 #mass of Jupiter in kg
 R_jup = 69911000 #radius of Jupiter in m
+rho_jup = 1.33 #density of Jupiter [g/cm3]
 AU = 149597870700 #AU in m
+M_earth = 5.972E24      #Mass of the Earth [kg]
+R_earth = 6371393 #earth radius [m]
+M_venus = 4.867E24 #mass of Venus [kg]
+M_mars = 6.39E23 #mass of mars [kg]
+M_saturn = 5.683E26 #mass of saturn [kg]
+M_neptune = 1.024E26 #mass of neptune [kg]
+M_uranus = 8.681E25 #mass of Uranus [kg]
+
+function init_solar_system_planets()
+  solar_system = []
+
+  venus = Planet()
+  venus.name = "Venus"
+  venus.mass = M_venus / M_jup
+  venus.distance = 0.72
+  venus.radius = 6051133 / R_jup
+  venus.period = 225
+  venus.density = 5.24 / rho_jup
+  venus.flux = 1366/venus.distance^2
+  push!(solar_system, venus)
+
+  earth = Planet()
+  earth.name = "Earth"
+  earth.mass = M_earth / M_jup
+  earth.distance = 1
+  earth.radius = R_earth / R_jup
+  earth.period = 365
+  earth.density = 5.51 / rho_jup
+  earth.flux = 1366/earth.distance^2
+  push!(solar_system, earth)
+
+  mars = Planet()
+  mars.name = "Mars"
+  mars.mass = M_mars / M_jup
+  mars.distance = 1.524
+  mars.radius = 3389278 / R_jup
+  mars.period = 687
+  mars.density = 3.39 / rho_jup
+  mars.flux = 1366/mars.distance^2
+  push!(solar_system, mars)
+
+  jupiter = Planet()
+  jupiter.name = "Jupiter"
+  jupiter.mass = 1
+  jupiter.distance = 5.2
+  jupiter.radius = 1
+  jupiter.period = 4332
+  jupiter.density = 1
+  jupiter.flux = 1366/jupiter.distance^2
+  push!(solar_system, jupiter)
+
+  saturn = Planet()
+  saturn.name = "Saturn"
+  saturn.mass = M_saturn / M_jup
+  saturn.distance = 9.5
+  saturn.radius = 58232000 / R_jup
+  saturn.period = 10585
+  saturn.density = 0.687 / rho_jup
+  saturn.flux = 1366/saturn.distance^2
+  push!(solar_system, saturn)
+
+  uranus = Planet()
+  uranus.name = "Uranus"
+  uranus.mass = M_uranus / M_jup
+  uranus.distance = 19.22
+  uranus.radius = 25361652 / R_jup
+  uranus.period = 30660
+  uranus.density = 1.27 / rho_jup
+  uranus.flux = 1366/uranus.distance^2
+  push!(solar_system, uranus)
+
+  neptune = Planet()
+  neptune.name = "Neptune"
+  neptune.mass = M_neptune / M_jup
+  neptune.distance = 30.1
+  neptune.radius = 24621354 / R_jup
+  neptune.period = 60225
+  neptune.density = 1.64 / rho_jup
+  neptune.flux = 1366/neptune.distance^2
+  push!(solar_system, neptune)
+
+  return solar_system
+end
+
 
 type Planet
   name::AbstractString
@@ -18,6 +105,8 @@ type Planet
   distance::Float64
   radius::Float64
   density::Float64
+  density_e_m::Float64
+  density_e_p::Float64
   temp::Float64
   stellar_temp::Float64
   stellar_mass::Float64
@@ -78,6 +167,8 @@ function create_planet_array()
         p.distance = orbital_dist[i,1]
         p.radius = planet_radii[i,1]
         p.density = planet_density[i,1]
+        p.density_e_p = planet_density[i,2] #positive density error
+        p.density_e_m = planet_density[i,3] #negative density error
         p.temp = planetary_temp[i,1]
         if p.stellar_rad > 0 && p.stellar_temp > 0 && p.distance > 0
           p.flux = (p.stellar_rad*695700099)^2*(5.67E-8)*(p.stellar_temp)^4/(p.distance*AU)^2
@@ -135,6 +226,48 @@ function show_small_planets()
   show()
 end
 
+function generate_rho_v_dist()
+  planets = create_planet_array()
+
+  y = []
+  x = []
+  z = []
+  x_err_p = []
+  x_err_m = []
+  for p in planets
+    if p.density > 0 && p.distance > 0 && p.mass > 0
+      push!(y,p.flux/1366)
+      push!(x,p.density*rho_jup)
+      push!(z,p.mass*M_jup/M_earth)
+
+      push!(x_err_m, p.density_e_m)
+      push!(x_err_p, p.density_e_p)
+    end
+  end
+
+
+
+  sc = scatter(x,y, c=z, norm=COL.LogNorm(vmin=minimum(z), vmax=maximum(z)), zorder=100)
+  cbar = colorbar(sc)
+  cbar[:ax][:set_ylabel]("Mass [Earth Masses]")
+
+  errorbar(x,y, xerr=(x_err_m,x_err_p), fmt="None", marker="None", mew=0, zorder=0)
+
+  sp = init_solar_system_planets()
+  for p in sp
+    plot([p.density*rho_jup], [p.flux/1366], label=p.name, marker="*", markersize=20, linestyle = "None")
+  end
+
+  legend(numpoints=1, loc="lower right")
+
+  xscale("log")
+  yscale("log")
+  title("Flux vs Density")
+  xlabel("Density [g/cc]")
+  ylabel("Relative Stellar Flux")
+  show()
+end
+
 function generate_flux_v_esc()
   planets = create_planet_array()
 
@@ -179,27 +312,119 @@ function generate_esc_v_dist()
   show()
 end
 
-function generate_vescDorb_v_rho()
+function generate_flux_v_vesc()
   planets = create_planet_array()
+
+  function vesc_vimp(mass, radius, period, distance)
+    vesc = (2*G*mass*M_jup/(radius*R_jup))^0.5 #[m/s]
+    vorb = 2*pi*distance*AU/(period*24*3600) #orbital velcity [m/s]
+    venc = 0.5*vorb
+    vimp = (vesc^2+venc^2)^0.5
+
+    return (vesc, vimp)
+  end
 
   y = []
   x = []
+  z = []
   for p in planets
     if p.density > 0 && p.mass > 0 && p.radius > 0 && p.period > 0 && p.distance > 0
-      vesc = (2*G*p.mass*M_jup/(p.radius*R_jup))^0.5 #[m/s]
-      vimp = 2*pi*p.distance*AU/(p.period*24*3600) #impact veloctiy ~ orbital velcity [m/s]
-      push!(y,vesc/vimp)
-      push!(x,p.density)
+      vesc, vimp = vesc_vimp(p.mass, p.radius, p.period, p.distance)
+      push!(y,p.flux/1366)
+      push!(x,vesc/1000)
+      push!(z,p.distance)
     end
   end
 
-  scatter(x,y)
+
+  sc = scatter(x,y, c=z, norm=COL.LogNorm(vmin=minimum(z), vmax=maximum(z)))
+  cbar = colorbar(sc)
+  cbar[:ax][:set_ylabel]("Distance [AU]")
+  #xscale("log")
+  #yscale("log")
+  title("Flux VS Escape Velocity")
+  xlabel("Escape Velocity [km/s]")
+  ylabel("Relative Stellar Heating")
+
+  sp = init_solar_system_planets()
+  for p in sp
+    vesc, vimp = vesc_vimp(p.mass, p.radius, p.period, p.distance)
+    plot([vesc/1000], [p.flux/1366], label=p.name, marker="*", markersize=20, linestyle = "None")
+  end
+
+  for p in planets
+    if p.name == "Kepler-10b"
+      vesc, vimp = vesc_vimp(p.mass, p.radius, p.period, p.distance)
+      plot([vesc/1000], [p.flux/1366], label=p.name, marker="v", markersize=20, linestyle = "None")
+    end
+
+    if p.name == "CoRoT-07"
+      vesc, vimp = vesc_vimp(p.mass, p.radius, p.period, p.distance)
+      plot([vesc/1000], [p.flux/1366], label=p.name, marker="s", markersize=20, linestyle = "None")
+    end
+  end
+
+  legend(numpoints=1, loc="upper left") #loc="upper left"
+
+  #annotate("local max", xy=(3, 1), xytext=(0.8, 0.95))
   xscale("log")
   yscale("log")
-  title("Density vs Impact Velocity / Escape Velocity")
-  xlabel("log(Density) [kg/m3]")
-  ylabel("Impact/Escape Velocity")
   show()
 end
 
-generate_vescDorb_v_rho()
+function generate_vescDorb_v_rho()
+  planets = create_planet_array()
+
+  function vesc_vimp(mass, radius, period, distance)
+    vesc = (2*G*mass*M_jup/(radius*R_jup))^0.5 #[m/s]
+    vorb = 2*pi*distance*AU/(period*24*3600) #orbital velcity [m/s]
+    venc = 0.5*vorb
+    vimp = (vesc^2+venc^2)^0.5
+
+    return (vesc, vimp)
+  end
+
+  y = []
+  x = []
+  z = []
+  for p in planets
+    if p.density > 0 && p.mass > 0 && p.radius > 0 && p.period > 0 && p.distance > 0
+      vesc, vimp = vesc_vimp(p.mass, p.radius, p.period, p.distance)
+      push!(y,vimp/vesc)
+      push!(x,p.density*rho_jup)
+      push!(z,p.flux/1366)
+    end
+  end
+
+
+  sc = scatter(x,y, c=z, norm=COL.LogNorm(vmin=minimum(z), vmax=maximum(z)))
+  cbar = colorbar(sc)
+  cbar[:ax][:set_ylabel]("Stellar Flux [Earth Solar Fluxes]")
+  xscale("log")
+  #yscale("log")
+  title("Density vs Impact Velocity / Escape Velocity")
+  xlabel("Density [g/cm3]")
+  ylabel("Impact/Escape Velocity")
+
+  sp = init_solar_system_planets()
+  for p in sp
+    vesc, vimp = vesc_vimp(p.mass, p.radius, p.period, p.distance)
+    plot([p.density*rho_jup], [vimp/vesc], label=p.name, marker="*", markersize=20, linestyle = "None")
+  end
+
+  for p in planets
+    if p.name == "Kepler-10b"
+      vesc, vimp = vesc_vimp(p.mass, p.radius, p.period, p.distance)
+      plot([p.density*rho_jup], [vimp/vesc], label=p.name, marker="v", markersize=20, linestyle = "None")
+    end
+  end
+
+  legend(numpoints=1) #loc="upper left"
+
+  #annotate("local max", xy=(3, 1), xytext=(0.8, 0.95))
+
+  show()
+end
+
+#generate_flux_v_vesc()
+generate_rho_v_dist()
